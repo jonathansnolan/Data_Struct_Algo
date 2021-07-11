3c3c8bbc4df0b0a31ff71d22a3f12f3273781343
Jonathan-Nolan <jonathan.nolan.jn@gmail.com>
2021-07-11 15:41:37 +0100

    Updated

--- Lesson2/test.py
+++ Lesson2/test.py
@@ -1,10 +1,25 @@
+
+#from LinkedListSetUp import *
+
 from LinkedListSetUp import element
 from LinkedListSetUp import Linkedlist
 
+#################################
+# NOTE:
+# You have to import the necessary classes individually it seems
+# Alternatively, you can use the following code:
+# from LinkedListSetUp import *
+
+################################
 # Set your Elements
+################################
+
 e1 = element("Jonathan")
 e2 = element("Nolan")
 
+################################
+# Make your LinkedList
+################################
 LL = Linkedlist(e1)
 LL.append(e2)
 
